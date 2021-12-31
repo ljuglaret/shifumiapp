1e26fed6e2d3c967d770fc53cefd225d8d0703a9
ljuglaret <ljuglaret@gmail.com>
2022-01-01 00:10:15 +0100

    Updates

---
+++ precache-manifest.6c7f8cbec08024188027bc365afd2519.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b248e5567ccfe9fe74f",
+    "url": "/shifumiapp/static/css/main.f1f2dfeb.chunk.css"
+  },
+  {
+    "revision": "3b248e5567ccfe9fe74f",
+    "url": "/shifumiapp/static/js/main.acde1ae6.chunk.js"
+  },
+  {
+    "revision": "27a6f17939870660575d",
+    "url": "/shifumiapp/static/js/runtime~main.c8395a08.js"
+  },
+  {
+    "revision": "a8dd4b6fa5a71e43bb35",
+    "url": "/shifumiapp/static/js/2.027070b8.chunk.js"
+  },
+  {
+    "revision": "c7768f9825758d119bef376a2e0e5b0f",
+    "url": "/shifumiapp/static/media/PFC.c7768f98.jpg"
+  },
+  {
+    "revision": "068c1f0dadb5be652e66e2f309163f08",
+    "url": "/shifumiapp/static/media/PFCLS.068c1f0d.jpg"
+  },
+  {
+    "revision": "f3add7ff2a778294a0d869a1f717b5e9",
+    "url": "/shifumiapp/static/media/poke.f3add7ff.jpg"
+  },
+  {
+    "revision": "43400a87f2cf52f9e27a74ee73d90b58",
+    "url": "/shifumiapp/index.html"
+  }
+];
